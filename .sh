37334243c4254c3ac25f2f9d811a1b4692618836
Dom Firinga <dominique.firinga@decathlon.com>
2017-11-22 14:25:42 +0100

    ajout de commande D2

---
+++ .sh
@@ -0,0 +1,6 @@
+test init fichier .sh
+cp old_file.txt new_file_copy.txt
+cp -R a_directory a_directory_copy
+echo “XXXX”
+touch empty_file.txt -> crée un fichier vierge totalement
+echo "Elementary, my dear Watson" > sherlock.txt
