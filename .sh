d194b31f3d046ce67717799188073dff7367da7a
Dom Firinga <dominique.firinga@decathlon.com>
2017-11-22 14:38:56 +0100

    2nd test commit avec pwd

--- .sh
+++ .sh
@@ -1,6 +1,1 @@
-test init fichier .sh
-cp old_file.txt new_file_copy.txt
-cp -R a_directory a_directory_copy
-echo “XXXX”
-touch empty_file.txt -> crée un fichier vierge totalement
-echo "Elementary, my dear Watson" > sherlock.txt
+new commands on the file
